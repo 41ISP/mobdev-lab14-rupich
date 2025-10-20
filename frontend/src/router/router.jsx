c33afd5d891fc4c8b5d8c94c5739bded33bfb107
Ira-rupich <rupich.ira@gmail.com>
2025-10-20 08:19:39 +0000

    rupich

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -36,6 +36,6 @@ export const router = createBrowserRouter([
     },
 ],
     {
-        basename: "/mobdev-lab13-rupich"
+        basename: "/mobdev-lab14-rupich"
     }
 )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   base: '/mobdev-lab13-rupich/'
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifestFilename: 'manifest.json',
+      includeManifestIcons: false,
+    }),
+  ],
+  base: '/mobdev-lab14-rupich/',
 })
